dc92e551812f66fb10f60ba28d073a09bb0cbd14
Claudio Álvarez <calvarez1@miuandes.cl>
2017-06-14 17:59:46 -0400

    The ideas table in the database lacked a required column 'orden'.

--- public/services/d3.js
+++ public/services/d3.js
@@ -18,7 +18,7 @@
                 var scriptTag = $document[0].createElement('script');
                 scriptTag.type = 'text/javascript';
                 scriptTag.async = true;
-                scriptTag.src = 'js/node_modules/d3/d3.js';
+                scriptTag.src = 'js/libs/d3.min.js';
                 scriptTag.onreadystatechange = function () {
                     if (this.readyState == 'complete') onScriptLoad();
                 }
